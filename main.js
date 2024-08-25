be2475af35fe04f2c802fc1a28f090c6e3bafd75
baxtiyorov2010 <torabaxtiyorov29@gmail.com>
2024-08-25 15:03:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let string = prompt("string tyupe digi malumotni kiriting")
+let number = prompt("number tyupe digi malumotni kiriting")
+let boolen = prompt("boolen tyupe digi malumotni kiriting")
+confirm("hamma malumotlaringiz togri kiritganizga ishonchingiz komilmi?")
+
+alert(`siz krithgan ${string} string typuga tegishli \n
+siz krithgan ${number} number typuga tegishli \n
+siz krithgan ${boolen} boolen typuga tegishli `)
